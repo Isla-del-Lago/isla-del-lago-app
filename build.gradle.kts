95239d0e1ab7f6d3066cceda137f86308511b56b
Camilo Lopez <camilino123@gmail.com>
2022-10-29 21:59:33 -0500

    Manifest updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,19 @@ allprojects {
 	tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
 		mainClass.set("isla.del.lago.shenglong.ApplicationKt")
 		archiveClassifier.set("boot")
+
+		manifest {
+			attributes["Main-Class"] = "isla.del.lago.shenglong.Application"
+		}
+
+		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+		from(sourceSets.main.get().output)
+
+		dependsOn(configurations.runtimeClasspath)
+		from({
+			configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+		})
 	}
 
 	tasks.withType<Test> {
